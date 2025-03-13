3dc7d39e51e1ac3779170d5962af3e46940ef715
Periicles <landescrew.scoot@gmail.com>
2025-03-13 21:04:47 +0100

    feat: Created repository for Brewery entity

---
+++ src/main/java/com/security/repository/BreweryRepository.java
@@ -0,0 +1,9 @@
+package com.security.repository;
+
+import com.security.entity.Brewery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BreweryRepository extends JpaRepository<Brewery, String> {
+}
