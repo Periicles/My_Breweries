d15276059bec500304cf3176d31be23f4c3c4fef
Periicles <landescrew.scoot@gmail.com>
2025-03-13 21:01:53 +0100

    refactor: created the route that fetches irish breweries and store them in db

--- src/main/java/com/security/service/BreweryService.java
+++ src/main/java/com/security/service/BreweryService.java
@@ -1,20 +1,55 @@
 package com.security.service;
 
 import com.security.dto.BreweryDTO;
+import com.security.entity.Brewery;
+import com.security.repository.BreweryRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BreweryService {
 
-    private static final String BREWERIES_API_URL = "https://api.openbrewerydb.org/v1/breweries?per_page=3";
+    @Autowired
+    private RestTemplate restTemplate;
 
-    public List<BreweryDTO> getBreweries() {
-        RestTemplate restTemplate = new RestTemplate();
-        BreweryDTO[] breweries = restTemplate.getForObject(BREWERIES_API_URL, BreweryDTO[].class);
-        return Arrays.asList(breweries);
+    @Autowired
+    private BreweryRepository breweryRepository;
+
+    public List<BreweryDTO> fetchAndSaveAllIrishBreweries() {
+        int page = 1;
+        int perPage = 50;
+        List<BreweryDTO> allBreweries = new ArrayList<>();
+
+        while (true) {
+            String url = "https://api.openbrewerydb.org/v1/breweries?by_country=ireland&page=" + page + "&per_page=" + perPage;
+            BreweryDTO[] breweriesDTO = restTemplate.getForObject(url, BreweryDTO[].class);
+            if (breweriesDTO == null || breweriesDTO.length == 0) {
+                break;
+            }
+            allBreweries.addAll(Arrays.asList(breweriesDTO));
+            // Save each brewery to the database
+            for (BreweryDTO breweryDTO : breweriesDTO) {
+                Brewery brewery = convertToEntity(breweryDTO);
+                breweryRepository.save(brewery);
+            }
+            // If the number of breweries returned is less than perPage, it means we've reached the last page
+            if (breweriesDTO.length < perPage) {
+                break;
+            }
+            page++;
+        }
+        return allBreweries;
+    }
+
+    private Brewery convertToEntity(BreweryDTO breweryDTO) {
+        Brewery brewery = new Brewery();
+        BeanUtils.copyProperties(breweryDTO, brewery);
+        return brewery;
     }
 }
